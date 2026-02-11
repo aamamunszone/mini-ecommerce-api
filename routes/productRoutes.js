6892a23d7b86df1a0d4079a747c77e723854f74d
Abdullah Al Mamun <aamamunszone@gmail.com>
2026-02-12 04:02:10 +0600

    feat: implement update and delete product functionality with admin access

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const {
   getProducts,
   createProduct,
+  updateProduct,
+  deleteProduct,
 } = require('../controllers/productController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+
+router
+  .route('/:id')
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 module.exports = router;
